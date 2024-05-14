5f0988db458b1160eeeefa65c30b9eb39aba0571
Felipe Vieira Sobral <xfelipesobral@gmail.com>
2024-05-14 12:06:59 -0300

    init

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+
+export default function App() {
+
+    return (
+        <View className='bg-gray-200 flex-1'>
+            <Text>App</Text>
+        </View>
+    )
+}
